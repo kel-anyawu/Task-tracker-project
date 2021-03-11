28ce168e9917b8c72b1a09a39aca524227af50a9
kel-anyawu <69680163+kel-anyawu@users.noreply.github.com>
2021-03-11 10:02:06 +0100

    rounding up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {useState} from "react";
 import './App.css';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import About from "./components/About";
 
 function App() {
   const [tasks, setTasks] = useState ([
@@ -25,20 +29,46 @@ function App() {
   }]
   )
 
+  // Add button
+  const [showAddTask, setShowAddTask, ] = useState(false);
+
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+
+  // Toggle a task
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ?
+    {...task, reminder: !task.reminder} : task))
+  }
+
   // Delete each task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   return (
+    <Router>
     <div className= "container" >
-      <Header title="Task Tracker"/>
+      <Header title="Task Tracker" onAdd = {() => setShowAddTask(!showAddTask)} showAdd = {showAddTask} />
+      <Route path="/" exact render={(props) => (
+        <>
+        {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ?(
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
-      ) : ("No tasks to show.")
-      
-      }
+      <Tasks tasks={tasks} onDelete={deleteTask}
+      onToggle={toggleReminder}/>
+      ) : ("No tasks to show.")}
+        </>
+      )}/>
+      <Route path="/about" component={About} />
+      <Footer />
     </div>
+    </Router>
   );
 }
 
